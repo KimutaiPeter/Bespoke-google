f6399398a3c750dd483d392b5ccc044128e84a6c
KimutaiPeter <kimutaipeterkibet@gmail.com>
2024-08-13 22:57:25 +0300

    initial

---
+++ content.js
@@ -0,0 +1,7 @@
+
+document.body.style.backgroundImage=`url(https://i.postimg.cc/t4ZQ4n31/Frame-4.png)`
+document.body.style.backgroundRepeat="space"
+window.onload = function () {
+    //document.body.style.backgroundColor='red'
+    console.log('Loaded')
+};
